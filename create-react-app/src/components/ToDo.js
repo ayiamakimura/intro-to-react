f4643446a5466a3eaea2a0b6f10b6ee4ccf1587b
ayiamakimura <yassinayia@gmail.com>
2018-10-07 09:40:46 +0200

    Checkpoint 8 ReactToDoEvents

--- create-react-app/src/components/ToDo.js
+++ create-react-app/src/components/ToDo.js
@@ -3,9 +3,12 @@
  class ToDo extends Component {
    render() {
      return (
-       <li>{ this.props.description }</li>
+       <li>
+         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+         <span>{this.props.description}</span>
+       </li>
      );
    }
- } 
+ }
 
  export default ToDo;
